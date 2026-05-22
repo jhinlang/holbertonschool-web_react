5ef3876c64bdd5f0be836c34cc574f6cb1fc02c3
jhinlang <julienhinlang@gmail.com>
2026-05-22 16:45:11 +0000

    feat: finishing task_4

---
+++ task_4/js/subjects/Java.ts
@@ -0,0 +1,21 @@
+import { Subjects } from './Subject';
+import './Teacher';
+
+export namespace Subjects {
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Java';
+    }
+
+    getAvailableTeacher(): string {
+      if (!this.teacher.experienceTeachingJava) {
+        return 'No available teacher';
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
---
+++ task_4/js/subjects/Subject.ts
@@ -0,0 +1,11 @@
+import './Teacher';
+
+export namespace Subjects {
+  export class Subject {
+    protected teacher!: Teacher;
+
+    setTeacher(teacher: Teacher): void {
+      this.teacher = teacher;
+    }
+  }
+}
